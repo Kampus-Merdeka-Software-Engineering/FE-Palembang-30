9d223164afdd9c9bf7613729a14601ee65a931bc
ppermadik <ppermadik195@gmail.com>
2023-10-09 23:40:43 +0800

    update register and login js

--- JS/script.js
+++ JS/script.js
@@ -16,6 +16,76 @@ iconClose.addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
+let isLogin=false;
+
+document.getElementById("login-form").addEventListener("submit", async function (e) {
+  e.preventDefault();
+  
+  // Gather user input
+  const username = document.getElementById("username-login").value;
+  const password = document.getElementById("password-login").value;
+
+  // Make a POST request to your API
+  const response = await fetch('https://be-palembang-30.vercel.app/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (response.status === 200) {
+    isLogin=true;
+    alert("Login successful");
+    // Optionally, redirect the user to the login page
+    window.location.href = '/index.html';
+  } else if (response.status === 401) {
+    alert("Invalid username or password");
+  } else {
+    alert("Registration failed. Please try again later.");
+  }
+});
+
+document.getElementById("registration-form").addEventListener("submit", async function (e) {
+  e.preventDefault();
+  
+  // Gather user input
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirm-password").value;
+  const agreeTerms = document.getElementById("agree-terms").checked;
+
+  // Validate input
+  if (!username || !email || !password || !confirmPassword || !agreeTerms) {
+    alert("Please fill in all fields and agree to the terms & conditions.");
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match.");
+    return;
+  }
+
+  // Make a POST request to your API
+  const response = await fetch('https://be-palembang-30.vercel.app/users/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, email, password }),
+  });
+
+  if (response.status === 201) {
+    alert("Registration successful");
+    // Optionally, redirect the user to the login page
+    window.location.href = '/login.html';
+  } else if (response.status === 409) {
+    alert("Username already exists");
+  } else {
+    alert("Registration failed. Please try again later.");
+  }
+});
 // btnPopup.addEventListener("click", () => {
 //   wrapper.classList.add("active-popup");
 // });
